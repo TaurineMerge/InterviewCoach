3e33166195df1006f2c39d351ac2b7c5a3f4d79b
Vladislav <plotnikov12341@gmail.com>
2025-09-24 00:36:39 +0300

    chore(project-config): update aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,11 +13,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@parser': path.resolve(__dirname, './src/parser'),
       '@logger': path.resolve(__dirname, './src/utils/logger'),
       '@file-validation': path.resolve(
         __dirname,
         './src/utils/file-validation',
       ),
+      '@id-generator': path.resolve(__dirname, './src/utils/id-generator'),
     },
   },
 });
