78a46b27415205f597b7045d4eadd03cb2f53fc4
Vladislav <plotnikov12341@gmail.com>
2025-09-23 18:42:36 +0300

    refactor(parser): implement common function for parsing files, change logger level from info to debug

--- src/parser/md-parser.ts
+++ src/parser/md-parser.ts
@@ -2,6 +2,24 @@ import { promises as fs } from 'fs';
 import { doesFileExist } from '@file-validation/file-validation.js';
 import { logger } from '@logger/logger.js';
 
+async function parseFile(filePath: string): Promise<string | null> {
+  if (!(await doesFileExist(filePath))) {
+    return null;
+  }
+
+  try {
+    logger.debug(`Parsing ${filePath}`);
+    const raw = await fs.readFile(filePath, 'utf-8');
+
+    logger.debug(`File ${filePath} parsed successfully`);
+
+    return raw;
+  } catch (error) {
+    logger.error(`Error while parsing ${filePath}: ${error}`);
+    return null;
+  }
+}
+
 export async function parseFileShort(
   filePath: string,
 ): Promise<{ short?: string; hasLong: boolean } | null> {
@@ -12,18 +30,14 @@ export async function parseFileShort(
     return null;
   }
 
-  try {
-    logger.info(`Parsing ${filePath}`);
-    const raw = await fs.readFile(filePath, 'utf-8');
+  const raw = await parseFile(filePath);
 
-    short = raw.match(/#\s*Короткий ответ\s+([\s\S]*?)(?=#|$)/)?.[1]?.trim();
-    longExists = /#\s*Длинный ответ/.test(raw);
-  } catch (error) {
-    logger.error(`Error while parsing ${filePath}: ${error}`);
+  if (!raw) {
     return null;
   }
 
-  logger.info(`File ${filePath} parsed successfully`);
+  short = raw.match(/#\s*Короткий ответ\s+([\s\S]*?)(?=#|$)/)?.[1]?.trim();
+  longExists = /#\s*Длинный ответ/.test(raw);
 
   return short ? { short, hasLong: longExists } : { hasLong: longExists };
 }
@@ -35,17 +49,13 @@ export async function parseFileLong(filePath: string): Promise<string | null> {
     return null;
   }
 
-  try {
-    logger.info(`Parsing ${filePath}`);
-    const raw = await fs.readFile(filePath, 'utf-8');
+  const raw = await parseFile(filePath);
 
-    long = raw.match(/#\s*Длинный ответ\s+([\s\S]*?)(?=#|$)/)?.[1]?.trim();
-  } catch (error) {
-    logger.error(`Error while parsing ${filePath}: ${error}`);
+  if (!raw) {
     return null;
   }
 
-  logger.info(`File ${filePath} parsed successfully`);
+  long = raw.match(/#\s*Длинный ответ\s+([\s\S]*?)(?=#|$)/)?.[1]?.trim();
 
   return long || null;
 }
