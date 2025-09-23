9a7b7989063d2d1d96ead7a257b18c37a89fd3d7
Vladislav <plotnikov12341@gmail.com>
2025-09-23 18:40:24 +0300

    chore(logs): change logger level from info to debug in file-validation

--- src/utils/file-validation/file-validation.ts
+++ src/utils/file-validation/file-validation.ts
@@ -3,14 +3,14 @@ import { logger } from '@/utils/logger/logger.js';
 
 export async function doesFileExist(filePath: string): Promise<boolean> {
   try {
-    logger.info(`Accessing ${filePath}`);
+    logger.debug(`Accessing ${filePath}`);
     await fs.access(filePath);
   } catch {
     logger.error(`File ${filePath} not found`);
     return false;
   }
 
-  logger.info(`File ${filePath} found successfully`);
+  logger.debug(`File ${filePath} found successfully`);
 
   return true;
 }
