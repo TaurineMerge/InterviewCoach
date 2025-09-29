37df4b0d8e7cf56bad91ee177706109b4dcf9e91
Vladislav <plotnikov12341@gmail.com>
2025-09-29 21:30:59 +0300

    feat(project-root): create main function

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { initBot } from './bot/initBot';
+import { runBot } from './bot/runBot';
+import { BotClient } from '@/services/bot-client';
+import { buildTree } from '@parser/tree-builder';
+import 'dotenv/config';
+import { QuestionSelector } from '@/services/question-selector';
+import { ProgressService } from '@/services/progress-service';
+import { MongoProgressRepository } from './infra/progress-repository';
+import { SessionManager } from '@/services/session-manager';
+import { RedisSessionStore } from './infra/session-repository';
+
+async function main() {
+  const fsTree = await buildTree(process.env.QUESTIONS_ROOT_DIR || '');
+
+  const progressRepository = new MongoProgressRepository();
+  const sessionRepository = new RedisSessionStore();
+
+  const progressService = new ProgressService(progressRepository);
+  const questionSelector = new QuestionSelector(progressService);
+  const sessionManager = new SessionManager(sessionRepository);
+
+  const bot = initBot(process.env.TG_BOT_TOKEN || '');
+  const botClient = new BotClient(sessionManager, questionSelector, fsTree);
+  runBot(bot, botClient, fsTree);
+}
+
+main();
