51c0cee6579f4d3b56961427953cf08e9e708f28
Vladislav <plotnikov12341@gmail.com>
2025-10-03 23:30:01 +0300

    feat(project-root): inject session configuration into RedisSessionStore

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,17 @@ import { ProgressService } from '@/services/progress-service';
 import { MongoProgressRepository } from './infra/progress-repository';
 import { SessionManager } from '@/services/session-manager';
 import { RedisSessionStore } from './infra/session-repository';
-import { getDatabase } from '@/config/database/database-config';
+import { getDatabase, closeDatabase } from '@/config/database/database-config';
+import { connectSession, closeSession } from './config/session/session-config';
 
 async function main() {
   const fsTree = await buildTree(process.env.QUESTIONS_ROOT_DIR || '');
 
   const db = getDatabase();
+  const session = await connectSession();
 
   const progressRepository = new MongoProgressRepository(db);
-  const sessionRepository = new RedisSessionStore();
+  const sessionRepository = new RedisSessionStore(session);
 
   const progressService = new ProgressService(progressRepository);
   const questionSelector = new QuestionSelector(progressService);
@@ -25,6 +27,18 @@ async function main() {
   const bot = initBot(process.env.TG_BOT_TOKEN || '');
   const botClient = new BotClient(sessionManager, questionSelector, fsTree);
   runBot(bot, botClient, fsTree);
+
+  process.on('SIGINT', async () => {
+    await closeSession();
+    await closeDatabase();
+    process.exit(0);
+  });
+
+  process.on('SIGTERM', async () => {
+    await closeSession();
+    await closeDatabase();
+    process.exit(0);
+  });
 }
 
 main();
